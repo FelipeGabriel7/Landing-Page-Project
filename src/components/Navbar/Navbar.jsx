1d05e91eb432cc6c202f6d1d824d4390c6cb293c
FelipeGabriel7 <felipegabfd@gmail.com>
2024-11-01 14:09:00 -0300

    feat: correction navbar mobile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import RuelaLogo from '../../assets/Rhuela_Queries_logo-removebg-preview.png';
 import { FaBars, FaTimes } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 export const NavbarComponent = () => {
     const [isOpen, setIsOpen] = useState(false);
 
@@ -15,7 +16,7 @@ export const NavbarComponent = () => {
                 <img src={RuelaLogo} alt="Ruela Logo" className="max-w-[150px]" />
             </div>
             <div className="hidden md:flex gap-10 p-4 text-xl">
-                <ul><a href="/" className='hover:text-yellow-400'>Home</a></ul>
+                <ul><Link href="/" className='hover:text-yellow-400'>Home</Link></ul>
                 <ul><a href="#about" className='hover:text-yellow-400'>Sobre</a></ul>
                 <ul><a href="#plus" className='hover:text-yellow-400'>Benefícios</a></ul>
             </div>
@@ -28,9 +29,9 @@ export const NavbarComponent = () => {
             {isOpen && (
                 <div className="absolute top-24 left-0 w-full flex justify-start items-start bg-gray-800 text-white md:hidden">
                     <ul className="flex flex-col items-start p-8 gap-8">
-                        <li><a href="#" className='hover:text-yellow-400'>Home</a></li>
-                        <li><a href="#" className='hover:text-yellow-400'>Sobre</a></li>
-                        <li><a href="#" className='hover:text-yellow-400'>Benefícios</a></li>
+                        <li><Link href="/" className='hover:text-yellow-400'>Home</Link></li>
+                        <li><a href="#about" className='hover:text-yellow-400'>Sobre</a></li>
+                        <li><a href="#plus" className='hover:text-yellow-400'>Benefícios</a></li>
                     </ul>
                 </div>
             )}
